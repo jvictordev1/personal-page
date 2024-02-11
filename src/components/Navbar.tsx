2af992da798cd3a4dd325cab177b51d1f58826c9
jvictordev1 <josevictorcruzrb@gmail.com>
2024-02-11 16:00:19 -0300

    fix: header

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import { MoonStar } from "lucide-react";
 
 export default function Navbar() {
   return (
-    <nav className="flex fixed w-full justify-between px-8 py-8 font-bold backdrop-blur-md lg:px-28">
-      <h1 className="font-semibold text-2xl">José Victor</h1>
-      <div className="hidden items-center space-x-20 text-xl lg:flex">
-        <div className="space-x-4">
+    <nav className="flex fixed w-full justify-between items-center px-8 py-8 font-bold backdrop-blur-md lg:px-28">
+      <h1 className="font-semibold text-lg lg:text-2xl">José Victor</h1>
+      <div className="hidden items-center space-x-20 text-xl md:flex">
+        <div className="space-x-4 md:text-base lg:text-xl">
           <a href="#home">Home</a>
           <a className="text-slate-700" href="#about-me">
             About me
